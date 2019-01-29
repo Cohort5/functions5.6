7cc07c32fbe2f2088273186bdbf04ee06a36d5bf
Paul Costa <paulcosta.student@careerdevs.com>
2019-01-29 01:34:57 +0000

    updating

---
+++ coding-challenge/first-challenge.js
@@ -0,0 +1,23 @@
+//Create a function with three items (numbers) from cliff
+
+//Excepts array of numbers apply 7% tax and 15% return total
+
+var billone = 50.00;
+var billtwo = 75.00;
+var billthree = 125.00;
+
+var finalBill = billone + billtwo + billthree;
+
+console.log("Your total bill is", finalBill, "dollars"); 
+
+// var finalBill = {
+    
+//     first:  50.00,
+//     second: 75.00,
+//     third: 125.00,
+    
+// };
+
+
+
+// console.log("Six"+ currencyAbbrev[5]);
